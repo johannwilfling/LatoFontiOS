c42bba7ffceecb479ab752892bd466941c0616a6
Johann Wilfling <johann.wilfling@gmail.com>
2016-04-16 17:10:44 +0200

    Added example label within Example app.

--- Example/LatoFontiOS/ViewController.swift
+++ Example/LatoFontiOS/ViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
+import LatoFontiOS
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        label.text = "This is Lato."
+        label.font = UIFont.latoFontWithSize(17.0)
     }
 
     override func didReceiveMemoryWarning() {
